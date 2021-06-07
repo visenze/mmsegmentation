22bac2e7c31c59763930627630f6165fcfd2ddb4
Tam Le <tam.le@visenze.com>
2021-06-07 11:25:12 +0800

    Add inference code

--- mmseg/apis/__init__.py
+++ mmseg/apis/__init__.py
@@ -1,9 +1,17 @@
 from .inference import inference_segmentor, init_segmentor, show_result_pyplot
-from .test import multi_gpu_test, single_gpu_test
+from .test import (multi_gpu_inference, multi_gpu_test, save_seg_result,
+                   single_gpu_inference, single_gpu_test)
 from .train import get_root_logger, set_random_seed, train_segmentor
 
 __all__ = [
-    'get_root_logger', 'set_random_seed', 'train_segmentor', 'init_segmentor',
-    'inference_segmentor', 'multi_gpu_test', 'single_gpu_test',
-    'show_result_pyplot'
+    "get_root_logger",
+    "set_random_seed",
+    "train_segmentor",
+    "init_segmentor",
+    "inference_segmentor",
+    "multi_gpu_test",
+    "single_gpu_test",
+    "show_result_pyplot",
+    "single_gpu_inference",
+    "save_seg_result",
 ]
--- mmseg/apis/test.py
+++ mmseg/apis/test.py
@@ -11,6 +11,20 @@ from mmcv.image import tensor2imgs
 from mmcv.runner import get_dist_info
 
 
+def save_seg_result(results, out_folder):
+    """Save the segmentation result in files in output folder."""
+    prog_bar = mmcv.ProgressBar(len(results))
+    for res in results:
+        filename = res["filename"]
+        seg = res["seg"]
+        seg = np.array(seg).squeeze()
+        seg = (seg * 255).astype(np.uint8)
+
+        # Saving as JPG changes the image
+        out_file = osp.join(out_folder, filename.rsplit(".", 1)[0] + ".png")
+        mmcv.imwrite(seg, out_file)
+        prog_bar.update()
+
 def np2tmp(array, temp_file_name=None):
     """Save ndarray to local numpy file.
 
@@ -102,6 +116,152 @@ def single_gpu_test(model,
             prog_bar.update()
     return results
 
+
+
+def single_gpu_inference(model,
+                    data_loader,
+                    show=False,
+                    out_dir=None,
+                    efficient_test=False,
+                    opacity=0.5):
+    """Test with single GPU.
+
+    Args:
+        model (nn.Module): Model to be tested.
+        data_loader (utils.data.Dataloader): Pytorch data loader.
+        show (bool): Whether show results during inference. Default: False.
+        out_dir (str, optional): If specified, the results will be dumped into
+            the directory to save output results.
+        efficient_test (bool): Whether save the results as local numpy files to
+            save CPU memory during evaluation. Default: False.
+        opacity(float): Opacity of painted segmentation map.
+            Default 0.5.
+            Must be in (0, 1] range.
+    Returns:
+        list: The prediction results.
+    """
+
+    model.eval()
+    results = []
+    dataset = data_loader.dataset
+    prog_bar = mmcv.ProgressBar(len(dataset))
+    for i, data in enumerate(data_loader):
+        with torch.no_grad():
+            result = model(return_loss=False, **data)
+            img_metas = data['img_metas'][0].data[0]
+            filename = img_metas[0]['ori_filename']
+            # Put result in a list for consistency
+            result = [{
+                'filename': filename,
+                'seg': result
+            }]
+
+        if show or out_dir:
+            img_tensor = data['img'][0]
+            img_metas = data['img_metas'][0].data[0]
+            imgs = tensor2imgs(img_tensor, **img_metas[0]['img_norm_cfg'])
+            assert len(imgs) == len(img_metas)
+
+            for img, img_meta in zip(imgs, img_metas):
+                h, w, _ = img_meta['img_shape']
+                img_show = img[:h, :w, :]
+
+                ori_h, ori_w = img_meta['ori_shape'][:-1]
+                img_show = mmcv.imresize(img_show, (ori_w, ori_h))
+
+                if out_dir:
+                    out_file = osp.join(out_dir, img_meta['ori_filename'])
+                else:
+                    out_file = None
+
+                model.module.show_result(
+                    img_show,
+                    result,
+                    palette=dataset.PALETTE,
+                    show=show,
+                    out_file=out_file,
+                    opacity=opacity)
+
+        if isinstance(result, list):
+            if efficient_test:
+                result = [np2tmp(_) for _ in result]
+            results.extend(result)
+        else:
+            if efficient_test:
+                result = np2tmp(result)
+            results.append(result)
+
+        batch_size = len(result)
+        for _ in range(batch_size):
+            prog_bar.update()
+    return results
+
+
+
+def multi_gpu_inference(model,
+                   data_loader,
+                   tmpdir=None,
+                   gpu_collect=False,
+                   efficient_test=False):
+    """Test model with multiple gpus.
+
+    This method tests model with multiple gpus and collects the results
+    under two different modes: gpu and cpu modes. By setting 'gpu_collect=True'
+    it encodes results to gpu tensors and use gpu communication for results
+    collection. On cpu mode it saves the results on different gpus to 'tmpdir'
+    and collects them by the rank 0 worker.
+
+    Args:
+        model (nn.Module): Model to be tested.
+        data_loader (utils.data.Dataloader): Pytorch data loader.
+        tmpdir (str): Path of directory to save the temporary results from
+            different gpus under cpu mode.
+        gpu_collect (bool): Option to use either gpu or cpu to collect results.
+        efficient_test (bool): Whether save the results as local numpy files to
+            save CPU memory during evaluation. Default: False.
+
+    Returns:
+        list: The prediction results.
+    """
+
+    model.eval()
+    results = []
+    dataset = data_loader.dataset
+    rank, world_size = get_dist_info()
+    if rank == 0:
+        prog_bar = mmcv.ProgressBar(len(dataset))
+    for i, data in enumerate(data_loader):
+        with torch.no_grad():
+            result = model(return_loss=False, rescale=True, **data)
+
+        img_metas = data['img_metas'][0].data[0]
+        filename = img_metas[0]['ori_filename']
+        # Put result in a list for consistency
+        result = [{
+            'filename': filename,
+            'seg': result
+        }]
+
+        if isinstance(result, list):
+            if efficient_test:
+                result = [np2tmp(_) for _ in result]
+            results.extend(result)
+        else:
+            if efficient_test:
+                result = np2tmp(result)
+            results.append(result)
+
+        if rank == 0:
+            batch_size = len(result)
+            for _ in range(batch_size * world_size):
+                prog_bar.update()
+
+    # collect results from all ranks
+    if gpu_collect:
+        results = collect_results_gpu(results, len(dataset))
+    else:
+        results = collect_results_cpu(results, len(dataset), tmpdir)
+    return results
 
 def multi_gpu_test(model,
                    data_loader,
