7c43a6926ec07b10dc10e67418e2dbeafd5f513d
Tam Le <tam.le@visenze.com>
2021-06-17 11:30:58 +0800

    refactor(training): Update distributed training script for docker

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
 
+set -Eeo pipefail
+
+trap cleanup SIGINT SIGTERM ERR EXIT
+
+cleanup() {
+    trap - SIGINT SIGTERM ERR EXIT
+}
+
+NUMGPUS=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
+
+if [[ -v CUDA_VISIBLE_DEVICES ]]; then
+    NUMGPUS=$(($(echo $CUDA_VISIBLE_DEVICES | tr -cd , | wc -c) + 1))
+fi
+
 CONFIG=$1
-GPUS=$2
 PORT=${PORT:-29500}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname "$0")/train.py $CONFIG --launcher pytorch ${@:3}
+python -m torch.distributed.launch --nproc_per_node=$NUMGPUS --master_port=$PORT \
+    $(dirname "$0")/train.py $CONFIG --launcher pytorch ${@:2}
